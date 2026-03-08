0457970aee66ab58f8a4bc0060640e528c2946ec
naginenidivyasri <naginenidivyasri4926@gmail.com>
2026-03-08 23:08:44 +0530

    Add files via upload

---
+++ landline page/script.js
@@ -0,0 +1,9 @@
+function orderNow()
+{
+alert("Welcome to Royal Taste Restaurant! Your order will be ready soon.");
+}
+
+function bookTable()
+{
+alert("Your table booking request has been sent!");
+}
